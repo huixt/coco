1a8b8b6390a2d4415bd6de2a4fdf4ecc1eda8e45
daimon@air <daijian1@qq.com>
2017-08-24 16:12:39 +0800

    首先初始化logger，避免有些包的根路径的log打不出来。

--- coco/__init__.py
+++ coco/__init__.py
@@ -8,9 +8,8 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.dirname(BASE_DIR)
 __version__ = '0.4.0'
 
-
+from . import logger
 from .exceptions import SSHError
 from .proxy import ProxyServer
 from .interactive import InteractiveServer
 from .app import Coco
-from . import logger
